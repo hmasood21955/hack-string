7ccafaa2f982dfb1865ae16c959988ee82a42422
hmasood21955 <hmasood878@gmail.com>
2023-12-20 00:19:34 +0500

    end

--- string_utils.h
+++ string_utils.h
@@ -6,3 +6,5 @@ char *replaceCharCopy(const char *s, char oldChar, char newChar);
 void removeChar(char *s, char c);
 char *removeCharCopy(const char *s, char c);
 char **lengthSplit(const char *s, int n);
+
+#endif  // STRING_UTILS_H
