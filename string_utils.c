ad6c4cdaa1f3ba0c83c751b2409c119506d3b313
hmasood21955 <hmasood878@gmail.com>
2023-12-20 00:37:18 +0500

    replaceChar

--- string_utils.c
+++ string_utils.c
@@ -8,3 +8,11 @@ int countSubstrings(const char *s, int n) {
     int len = strlen(s);
     return (len + n - 1) / n;
 }
+void replaceChar(char *s, char oldChar, char newChar) {
+    while (*s != '\0') {
+        if (*s == oldChar) {
+            *s = newChar;
+        }
+        s++;
+    }
+}
