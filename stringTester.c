06daf4db7271d134dab6a3b0b561ec676b4e37a6
hmasood21955 <hmasood878@gmail.com>
2023-12-20 01:46:11 +0500

    Test replaceCharCopy

--- stringTester.c
+++ stringTester.c
@@ -7,3 +7,7 @@ int main() {
     char testStr1[] = "Hello, World!";
     replaceChar(testStr1, 'l', 'L');
     printf("replaceChar: %s\n", testStr1);
+    const char testStr2[] = "Hello, World!";
+    char *newStr = replaceCharCopy(testStr2, 'o', 'O');
+    printf("replaceCharCopy: %s\n", newStr);
+    free(newStr);
