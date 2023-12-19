3797ea6e312c0ac471d63ca321a641e89385da1c
hmasood21955 <hmasood878@gmail.com>
2023-12-20 00:19:20 +0500

    lengthSplit

--- string_utils.h
+++ string_utils.h
@@ -5,3 +5,4 @@ void replaceChar(char *s, char oldChar, char newChar);
 char *replaceCharCopy(const char *s, char oldChar, char newChar);
 void removeChar(char *s, char c);
 char *removeCharCopy(const char *s, char c);
+char **lengthSplit(const char *s, int n);
