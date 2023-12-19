8b96128dd1f477d6b7fc74742ea43106bf7eb8e5
hmasood21955 <hmasood878@gmail.com>
2023-12-20 01:37:30 +0500

    Test replaceChar

--- stringTester.c
+++ stringTester.c
@@ -2,3 +2,8 @@
 #include <stdlib.h>
 #include "string_utils.h"
 
+int main() {
+    
+    char testStr1[] = "Hello, World!";
+    replaceChar(testStr1, 'l', 'L');
+    printf("replaceChar: %s\n", testStr1);
