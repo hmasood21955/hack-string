62121538c9e9d4a7f0779f09bd0404a47ea1da7b
hmasood21955 <hmasood878@gmail.com>
2023-12-20 00:18:32 +0500

    removeCharCopy

--- string_utils.h
+++ string_utils.h
@@ -4,3 +4,4 @@
 void replaceChar(char *s, char oldChar, char newChar);
 char *replaceCharCopy(const char *s, char oldChar, char newChar);
 void removeChar(char *s, char c);
+char *removeCharCopy(const char *s, char c);
